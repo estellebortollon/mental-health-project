56342069de6abcf86dfe90be614c5de7dcd52ad2
Lucie Boyer <lucie-boyer@hotmail.com>
2025-12-09 13:19:15 +0000

    Correction erreur sur évolution en pourcentage pour burden table

--- models/intermediate/burden_country_evol.sql
+++ models/intermediate/burden_country_evol.sql
@@ -39,51 +39,63 @@ final as (
 
         round(dalys_depressive_disorders, 4) as dalys_depressive_disorders,
         round(
-            dalys_depressive_disorders - lag(dalys_depressive_disorders) over (
+            100 * (dalys_depressive_disorders - lag(dalys_depressive_disorders) over (
+                partition by entity order by year
+            )) / lag(dalys_depressive_disorders) over (
                 partition by entity order by year
             ),
             4
-        ) as depressive_pp_change,
+        ) as depressive_pct_change,
 
         round(dalys_schizophrenia, 4) as dalys_schizophrenia,
         round(
-            dalys_schizophrenia - lag(dalys_schizophrenia) over (
+            100 * (dalys_schizophrenia - lag(dalys_schizophrenia) over (
+                partition by entity order by year
+            )) / lag(dalys_schizophrenia) over (
                 partition by entity order by year
             ),
             4
-        ) as schizophrenia_pp_change,
+        ) as schizophrenia_pct_change,
 
         round(dalys_bipolar_disorder, 4) as dalys_bipolar_disorder,
         round(
-            dalys_bipolar_disorder - lag(dalys_bipolar_disorder) over (
+            100 * (dalys_bipolar_disorder - lag(dalys_bipolar_disorder) over (
+                partition by entity order by year
+            )) / lag(dalys_bipolar_disorder) over (
                 partition by entity order by year
             ),
             4
-        ) as bipolar_pp_change,
+        ) as bipolar_pct_change,
 
         round(dalys_eating_disorders, 4) as dalys_eating_disorders,
         round(
-            dalys_eating_disorders - lag(dalys_eating_disorders) over (
+            100 * (dalys_eating_disorders - lag(dalys_eating_disorders) over (
+                partition by entity order by year
+            )) / lag(dalys_eating_disorders) over (
                 partition by entity order by year
             ),
             4
-        ) as eating_pp_change,
+        ) as eating_pct_change,
 
         round(dalys_anxiety_disorders, 4) as dalys_anxiety_disorders,
         round(
-            dalys_anxiety_disorders - lag(dalys_anxiety_disorders) over (
+            100 * (dalys_anxiety_disorders - lag(dalys_anxiety_disorders) over (
+                partition by entity order by year
+            )) / lag(dalys_anxiety_disorders) over (
                 partition by entity order by year
             ),
             4
-        ) as anxiety_pp_change,
+        ) as anxiety_pct_change,
 
         round(total_burden, 4) as total_burden,
         round(
-            total_burden - lag(total_burden) over (
+            100 * (total_burden - lag(total_burden) over (
+                partition by entity order by year
+            )) / lag(total_burden) over (
                 partition by entity order by year
             ),
             4
-        ) as total_pp_change
+        ) as total_pct_change
 
     from base
 
